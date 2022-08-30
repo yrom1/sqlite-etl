0b35022b3e57ebecd0e40bd47238a713bd29d6c7
yrom1 <ryanm.inbox@gmail.com>
2022-08-30 18:28:08 -0400

    Load data into db

--- load.py
+++ load.py
@@ -1,11 +1,17 @@
 import pandas as pd
+import db
 
-df = pd.read_csv("archive.txt", sep=" ", thousands=",")
-df = df.reindex(columns=["date", "key", "value"])
-df.to_csv("archive2.csv", index=False)
+# --- EXTRACT TRANSFORM ---
+# df = pd.read_csv("archive.txt", sep=" ", thousands=",")
+# df = df.reindex(columns=["date", "key", "value"])
+# df.to_csv("archive2.csv", index=False)
 
-with open("archive2.csv", "r") as f:
-    content = f.readlines()
+# with open("archive2.csv", "r") as f:
+#     content = f.readlines()
 
-with open("archive.csv", "a") as f:
-    f.write("".join(content[1:]))
+# with open("archive.csv", "a") as f:
+#     f.write("".join(content[1:]))
+
+# --- LOAD ---
+data = pd.read_csv("archive.csv").drop_duplicates(["date", "key"]).to_numpy().tolist()
+db.insert_data(data)
