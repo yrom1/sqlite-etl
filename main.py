cb15a4e243210a19def8660805e36a0db957e7e0
yrom1 <ryanm.inbox@gmail.com>
2022-08-30 17:45:43 -0400

    Simplify just use db file

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import sqlite3
-from subprocess import run
 
-
-def load_db() -> None:
-    run("sqlite3 data.db < data.sql", shell=True)
-
-
-def save_db() -> None:
-    run("sqlite3 data.db .dump > data.sql", shell=True)
-    run("rm data.db", shell=True)
+DATABASE = "data.db"
 
 
 def create_schema() -> None:
-    # NOTE this is not written to be idempotent if the schema already exists
-    con = sqlite3.connect(f"data.db")
+    con = sqlite3.connect(DATABASE)
     cur = con.cursor()
     # sqlite has no DATE, https://www.sqlite.org/datatype3.html
     cur.execute(
         """
-    CREATE TABLE data (
+    CREATE TABLE IF NOT EXISTS data (
         date TEXT(10),
         key VARCHAR(255),
         value INT,
@@ -31,16 +22,13 @@ def create_schema() -> None:
 
 
 def insert_data(data: list[tuple[str, str, int]]) -> None:
-    con = sqlite3.connect(f"data.db")
+    con = sqlite3.connect(DATABASE)
     cur = con.cursor()
-    cur.executemany("INSERT INTO data VALUES (?, ?, ?)", data)
+    cur.executemany("INSERT INTO data (date, key, value) VALUES (?, ?, ?)", data)
     con.commit()
     con.close()
 
 
 if __name__ == "__main__":
-    load_db()
     create_schema()
-    # insert_data(...) TODO
-    save_db()
-    print("Done 🍰!")
+    print("Schema created 🍰!")
