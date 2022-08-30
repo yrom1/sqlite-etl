622939ee69e797c26562efba8ffaa7187718b9bf
yrom1 <ryanm.inbox@gmail.com>
2022-08-30 18:10:42 -0400

    Commit before changing data

--- load.py
+++ load.py
@@ -3,3 +3,9 @@ import pandas as pd
 df = pd.read_csv("archive.txt", sep=" ")
 df = df.reindex(columns=["date", "key", "value"])
 df.to_csv("archive2.csv", index=False)
+
+with open("archive2.csv", "r") as f:
+    content = f.readlines()
+
+with open("archive.csv", "a") as f:
+    f.write("".join(content[1:]))
