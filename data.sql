315827459c96170febe2173e3a589bcb03792eaf
yrom1 <yrom1@users.noreply.github.com>
2022-08-30 21:20:13 +0000

    Action update

--- data.sql
+++ data.sql
@@ -1,10 +1,3 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE data (
-        date TEXT(10),
-        key VARCHAR(255),
-        value INT,
-        PRIMARY KEY (date, key)
-    );
-INSERT INTO data VALUES('2022-08-30','banana',42);
 COMMIT;
